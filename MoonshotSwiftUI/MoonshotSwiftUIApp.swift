c20e10401e4044a07bc5cf89031e88d1abf5457b
Anastasiia Veremiichyk <anastasia.veremi@gmail.com>
2022-09-26 23:54:47 +0200

    Project set up.

---
+++ MoonshotSwiftUI/MoonshotSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotSwiftUIApp.swift
+//  MoonshotSwiftUI
+//
+//  Created by Anastasiia Veremiichyk on 26/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
