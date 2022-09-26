8fac0c4a219f5aab8b7e7933701b72e6e5893a1a
Anastasiia Veremiichyk <anastasia.veremi@gmail.com>
2022-09-27 00:16:54 +0200

    Added geometry reader.

--- MoonshotSwiftUI/ContentView.swift
+++ MoonshotSwiftUI/ContentView.swift
@@ -9,11 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Image("conrad")
-            .resizable()
-            .scaledToFit()
-            .frame(width: 300, height: 300)
-    //        .clipped()
+        GeometryReader { geo in
+            Image("conrad")
+                .resizable()
+                .scaledToFit()
+                .frame(width: geo.size.width * 0.8, height: 300)
+        //        .clipped()
+            // If you ever want to center a view inside a GeometryReader, rather than aligning to the top-left corner, add a second frame that makes it fill the full space of the container, like this
+            // .frame(width: geo.size.width, height: geo.size.height)
+        }
     }
 }
 
