4b5bf7b0221f73578333feb8cdbc013af10a0124
Anastasiia Veremiichyk <anastasia.veremi@gmail.com>
2022-10-02 17:12:52 +0200

    Added CrewRole as a nested struct of Mission struct for better context.

--- MoonshotSwiftUI/Models/Mission.swift
+++ MoonshotSwiftUI/Models/Mission.swift
@@ -8,6 +8,11 @@
 import Foundation
 
 struct Mission: Codable, Identifiable {
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+
     let id: Int
     let launchDate: String?
     let crew: [CrewRole]
