8b9ef2a9322378ece001dfa904cd8f1dfa737ef2
Anastasiia Veremiichyk <anastasia.veremi@gmail.com>
2022-10-02 16:52:14 +0200

    Added Astronaut model and decoding.

---
+++ MoonshotSwiftUI/Models/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  MoonshotSwiftUI
+//
+//  Created by Anastasiia Veremiichyk on 02/10/2022.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
